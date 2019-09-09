f33bea784654821e24e23d968ed4a598087e6075
flickerstop <maclean.joey@gmail.com>
2019-09-09 06:46:17 -0400

    Changed texture sizes

--- src/frontend/js/textures.js
+++ src/frontend/js/textures.js
@@ -3,232 +3,232 @@ let textures = [
         "name": "Solid Rock",
         "id":"solidRock1Texture",
         "path": "./images/dnd/textures/solid_rock_1.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Solid Rock",
         "id":"solidRock2Texture",
         "path": "./images/dnd/textures/solid_rock_2.jpg",
-        "dimensions":1200
+        "dimensions":300
     },{
         "name": "Solid Rock",
         "id":"solidRock3Texture",
         "path": "./images/dnd/textures/solid_rock_3.jpg",
-        "dimensions":1200
+        "dimensions":300
     },{
         "name": "Dark Rock Tiles",
         "id":"rockTilesDarkTexture",
         "path": "./images/dnd/textures/rock_tiles_dark.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Mossy Rock Tiles",
         "id":"rockTilesMossyTexture",
         "path": "./images/dnd/textures/rock_tiles_mossy.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Light Rock Tiles",
         "id":"rockTilesLightTexture",
         "path": "./images/dnd/textures/rock_tiles_light.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Dark Mossy Rock Tiles",
         "id":"rockTilesDarkMossyTexture",
         "path": "./images/dnd/textures/rock_tiles_dark_mossy.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Light Mossy Rock Tiles",
         "id":"rockTilesLightMossyTexture",
         "path": "./images/dnd/textures/rock_tiles_light_mossy.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Flat Stone Floor",
         "id":"flatStoneTexture",
         "path": "./images/dnd/textures/flat_stone_floor.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Dark Gray Brick",
         "id":"darkGrayBrickTexture",
         "path": "./images/dnd/textures/gray_brick_dark.jpg",
-        "dimensions":800
+        "dimensions":200
     },{
         "name": "Light Gray Brick",
         "id":"lightGrayBrickTexture",
         "path": "./images/dnd/textures/gray_brick_light.jpg",
-        "dimensions":800
+        "dimensions":200
     },{
         "name": "Red Herringbone",
         "id":"redHerringboneTexture",
         "path": "./images/dnd/textures/herringbone_red.jpg",
-        "dimensions":600
+        "dimensions":100
     },{
         "name": "Gray Herringbone",
         "id":"grayHerringboneTexture",
         "path": "./images/dnd/textures/herringbone_gray.jpg",
-        "dimensions":600
+        "dimensions":100
     },{
         "name": "Dark Cobblestone Arc",
         "id":"darkCobblestoneArcTexture",
         "path": "./images/dnd/textures/cobblestone_arc_dark.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Light Cobblestone Arc",
         "id":"lightCobblestoneArcTexture",
         "path": "./images/dnd/textures/cobblestone_arc_light.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Cobblestone",
         "id":"cobblestoneTexture",
         "path": "./images/dnd/textures/Cobblestone.jpg",
-        "dimensions":600
+        "dimensions":300
     },{
         "name": "Cobblestone Bricks",
         "id":"cobblestoneBricksTexture",
         "path": "./images/dnd/textures/cobblestone_bricks.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Cobblestone Bricks Dark",
         "id":"cobblestoneBricksDarkTexture",
         "path": "./images/dnd/textures/cobblestone_bricks_dark.jpg",
-        "dimensions":600
+        "dimensions":200
     },{
         "name": "Dark Square Tiles",
         "id":"squareTilesDarkTexture",
         "path": "./images/dnd/textures/square_tiles_dark.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Light Square Tiles",
         "id":"squareTilesLightTexture",
         "path": "./images/dnd/textures/square_tiles_light.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Red Square Tiles",
         "id":"squareTilesRedTexture",
         "path": "./images/dnd/textures/square_tiles_Red.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Square Tiles Pattern",
         "id":"squareTilesPattern1Texture",
         "path": "./images/dnd/textures/square_tiles_pattern_1.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Square Tiles Pattern",
         "id":"squareTilesPattern2Texture",
         "path": "./images/dnd/textures/square_tiles_pattern_2.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Square Tiles Pattern",
         "id":"squareTilesPattern3Texture",
         "path": "./images/dnd/textures/square_tiles_pattern_3.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Square Tiles Pattern",
         "id":"squareTilesPattern4Texture",
         "path": "./images/dnd/textures/square_tiles_pattern_4.jpg",
-        "dimensions":800
+        "dimensions":100
     },{
         "name": "Mud",
         "id":"mudTexture",
         "path": "./images/dnd/textures/mud.jpg",
-        "dimensions":1200
+        "dimensions":200
     },{
         "name": "Dirt",
         "id":"dirtTexture",
         "path": "./images/dnd/textures/dirt.jpg",
-        "dimensions":3200
+        "dimensions":200
     },{
         "name": "Gravel",
         "id":"gravelTexture",
         "path": "./images/dnd/textures/gravel.jpg",
-        "dimensions":3200
+        "dimensions":400
     },{
         "name": "Sand",
         "id":"sandTexture",
         "path": "./images/dnd/textures/sand.jpg",
-        "dimensions":3200
+        "dimensions":800
     },{
         "name": "Dark Grass",
         "id":"darkGrassTexture",
         "path": "./images/dnd/textures/grass_dark.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Bright Grass",
         "id":"brightGrassTexture",
         "path": "./images/dnd/textures/grass_bright.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Rocky Grass",
         "id":"rockyGrassTexture",
         "path": "./images/dnd/textures/grass_rocky.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Grass",
         "id":"grassTexture",
         "path": "./images/dnd/textures/grass.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Dirty Grass",
         "id":"dirtGrassTexture",
         "path": "./images/dnd/textures/grass_dirt.jpg",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Wooden Floor",
         "id":"wood1Texture",
         "path": "./images/dnd/textures/wood_1.jpg",
-        "dimensions":3200
+        "dimensions":400
     },{
         "name": "Wooden Floor",
         "id":"wood2Texture",
         "path": "./images/dnd/textures/wood_2.jpg",
-        "dimensions":3200
+        "dimensions":400
     },{
         "name": "Wooden Floor",
         "id":"wood3Texture",
         "path": "./images/dnd/textures/wood_3.jpg",
-        "dimensions":3200
+        "dimensions":400
     },{
         "name": "Water",
         "id":"waterTexture",
         "path": "./images/dnd/textures/water.jpg",
-        "dimensions":3200
+        "dimensions":1600
     },{
         "name": "Snow",
         "id":"snowTexture",
         "path": "./images/dnd/textures/snow.jpg",
-        "dimensions":1200
+        "dimensions":800
     },{
         "name": "Snow Overlay",
         "id":"snowOverlayTexture",
         "path": "./images/dnd/textures/snow_overlay.png",
-        "dimensions":1200
+        "dimensions":600
     },{
         "name": "Cracked Ice",
         "id":"crackedIceTexture",
         "path": "./images/dnd/textures/cracked_ice.jpg",
-        "dimensions":1200
+        "dimensions":400
     },{
         "name": "Ice",
         "id":"iceTexture",
         "path": "./images/dnd/textures/ice.jpg",
-        "dimensions":1200
+        "dimensions":400
     },{
         "name": "Lava",
         "id":"lavaTexture",
         "path": "./images/dnd/textures/lava.jpg",
-        "dimensions":1200
+        "dimensions":400
     },{
         "name": "Cooled Lava",
         "id":"cooledLavaTexture",
         "path": "./images/dnd/textures/lava_cooled.jpg",
-        "dimensions":1200
+        "dimensions":400
     },{
         "name": "Lava Rocks",
         "id":"lavaRocksTexture",
         "path": "./images/dnd/textures/lava_rocks.jpg",
-        "dimensions":1200
+        "dimensions":300
     },{
         "name": "Green Lava Rocks",
         "id":"greenLavaRocksTexture",
         "path": "./images/dnd/textures/lava_rocks_green.jpg",
-        "dimensions":1200
+        "dimensions":300
     }
 ]
 
